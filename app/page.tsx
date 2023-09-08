6fc01c35e722f1f5a635ab9c8f612cf04a8a63b8
kristinmc42 <mccollum.k.j@gmail.com>
2023-09-08 19:12:37 -0400

    edited home page to fix error

--- app/page.tsx
+++ app/page.tsx
@@ -19,7 +19,7 @@ export default function Home() {
         {categories.map((category: string, index: number) => {
           return (
             <li key={index}>
-              <Link href={`/collections/${category}`}>{category}</Link>
+              <p>{category}</p>
             </li>
           );
         })}
